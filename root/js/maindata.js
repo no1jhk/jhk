3e1d31a353fd3d32a582b5d57c98ed8b088b4fd9
Gdj <turfrain@yahoo.co.kr>
2023-05-12 17:19:53 +0900

    [DJ]기본 작업파일

--- root/js/maindata.js
+++ root/js/maindata.js
@@ -1,6 +1,6 @@
 var mainData = [
   {
-    id        : "#60",
+    id        : "60",
     title     : "엔씨소프트 게임이벤트 프로모션", 
     title_e   : "NCSOFT GAME EVENT PROMOTION",
     title_k   : "(주)엔씨소프트 plaync에 등록된 게임 이벤트 프로모션 작업",
@@ -11,34 +11,34 @@ var mainData = [
     image_off : "./img/ncsoft_bg.png",
     image_on  : "./img/ncsoft_bg1.png",
     info : [
-      {"작업내용": "(주)엔씨소프트 plaync에 등록된 게임 이벤트 프로모션 작업"},
-      {"작업기간": "2017.05 - 2019.02"},
-      {"고객사"  : "(주)엔씨소프트 plaync에 등록된 게임 이벤트 프로모션 작업"},
-      {"URL"     : "http://aion.plaync.com/events/2018/181221_secret"},
+      ["작업내용", "(주)엔씨소프트 plaync에 등록된 게임 이벤트 프로모션 작업"],
+      ["작업기간", "2017.05 - 2019.02"],
+      ["고객사"  , "엔씨소프트"],
+      ["URL"    , "http://aion.plaync.com/events/2018/181221_secret"],
     ],
     desc_text : "2017년 5월 운영부문의 가장 큰 고객사인 엔씨소프트의 프로젝트에 참여하게 되어 2019년 현재까지 70여건의 게임프로모션 세트를 제작하였습니다. 엔씨소프트의 온라인 게임 프로모션을 제작하여 사용자의 관심과 호기심을 유발하고 게임 활성화에 이바지합니다.",
-    desc_logo : "",
-    desc_image : "img/sub_cont_aion_secret.png"
+    desc_device : "",
+    desc_image : "../img//sub_cont_aion_secret.png"
   },
   {
-    id        : "#46",
-    title     : "리니지1 - 게임이벤트 프로모션", 
-    title_e   : "NCSOFT LINEAGE1 EVENT PROMOTION",
+    id        : "46",
+    title     : "LGU+ 3D프린터 웹사이트 구축", 
+    title_e   : "LGU+ 3D프린터 웹사이트 구축",
     title_k   : "(주)엔씨소프트 plaync에 등록된 게임 이벤트 프로모션 작업",
-    link      : "sub_ncsoft.html", 
+    link      : "sub_3dp.html", 
     logo_alt  : "LINEAGE1", 
-    logo_off  : "./img/lineage1_logo0.png",
-    logo_on   : "./img/lineage1_logo1.png", 
-    image_off : "./img/lineage1_bg.png",
-    image_on  : "./img/lineage1_bg1.png",
+    logo_off  : "./img/webhard_logo0.png",
+    logo_on   : "./img/webhard_logo1.png", 
+    image_off : "./img/3dp_bg.png",
+    image_on  : "./img/3dp_bg1.png",
     info : [
-      {"작업내용": "(주)엔씨소프트 plaync에 등록된 게임 이벤트 프로모션 작업"},
-      {"작업기간": "2017.05 - 2019.02"},
-      {"고객사"  : "(주)엔씨소프트 plaync에 등록된 게임 이벤트 프로모션 작업"},
-      {"URL"     : "http://aion.plaync.com/events/2018/181221_secret"},
+      ["작업내용" , "LGU+ 3D프린터 웹사이트 구축"],
+      ["작업기간" , "	2016.01 - 2016.02"],
+      ["고객사"   , "LGU+ 기업클라우드사업팀"],
     ],
-    desc_text : "2017년 5월 운영부문의 가장 큰 고객사인 엔씨소프트의 프로젝트에 참여하게 되어 2019년 현재까지 70여건의 게임프로모션 세트를 제작하였습니다. 엔씨소프트의 온라인 게임 프로모션을 제작하여 사용자의 관심과 호기심을 유발하고 게임 활성화에 이바지합니다.",
-    desc_image : "img//sub_cont_aion_secret.png"
+    desc_text : "3D프린터로 제작된 상품들을 판매하는 쇼핑몰 형태의 웹사이트를 LGU+ 웹하드에서 진행하게 되었습니다. 3D프린터를 처음 접하는 사용자들이 대부분이므로 단순 쇼핑몰 UI에 국한되지 않게, 고객 친화적인 UI 구성이 필요했습니다. 견적요청을 통한 제작의뢰 프로세스가 이해하기 쉽고 간결하게 표현해서 이용자가 어렵게 느끼지 않도록 하는데 중점을 두었습니다. 최신 트랜드를 반영하여 큰사이즈의 폰트사용과 넓찍한 여백의 구성으로 시안을 작업하였습니다.",
+    desc_device  : "../img/c_logo_lgu.png",
+    desc_image   : "../img/sub_cont_aion_secret.png"
   }
 
 ] 
